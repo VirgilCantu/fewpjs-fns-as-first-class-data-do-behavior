600b0fc7d094dc766e9a6019b605f2ed17855b4d
VirgilCantu <cantu.sampsom@gmail.com>
2021-03-15 19:25:46 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+  let parsedTime = parseInt(timeString)
+
+  if (parsedTime < 12) {
+    return "Good Morning"
+  } else if (parsedTime > 17) {
+    return "Good Evening"
+  } else {
+    return "Good Afternoon"
+  }
+}
+
+function displayMessage(message) {
+  let greeting = document.getElementById("greeting")
+  greeting.innerText = message
+}
+
+
+
+
 /* Write your implementation of displayMessage() */
